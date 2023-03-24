575e84bf242cf26fe3f27b951ceb200dbef3a65c
NICHOLAS FLEETWOOD <nic-fleetwood@behr.travel>
2023-03-24 17:13:13 -0400

    Add project files.

---
+++ ModalTestingIOS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ModalTestingIOS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
